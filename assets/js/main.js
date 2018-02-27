1b9a478828502afc73da37a5ca03ba4026afc29b
camilabelen <ccornejoss@gmail.com>
2018-02-27 15:41:22 -0300

    actualizacion html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,11 @@
-$(document).ready(function(){  
-
-});
-
-
- /*
-  $(window).on('scroll', function() {
-    $(this).height();
-    $(this).scrollTop();
-    if (top > height) {
-      if (!btn.is(':visible')) {
-        btn.show();
-      }
-    } else {
-      btn.hide();
-    }
-  });*/
-
-var tStart = 50 // Start transition 100px from top
-var tEnd = 3000   // End at 500px
-var cStart = [120, 61, 160]  // Gold
-var cEnd = [28, 131, 218]   // Lime
-var cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
-
-  $(document).scroll(function() {
-        var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
-        p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
-        var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
-        $("body").css('background-color', 'rgb(' + cBg.join(',') +')');
-    });
-  
-  // Botón volver al inicio
+$(document).ready(function(){ 
   var btn = $('#top');
   btn.on('click', function() {
     $('html, body').animate({
       scrollTop: 0
     }, 'slow');
   });
-
+});
 
 
 // Animación texto tipeo
@@ -51,28 +20,29 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+
+  var one = "#3CDCB0",
+  var two = "#38C7AB",
+  var three = "#34AFA6",
+  var four = "#32A1A3",
+  var five = "#2E8D9E";
 */
+ /*
+  $(window).on('scroll', function() {
+    $(this).height();
+    $(this).scrollTop();
+    if (top > height) {
+      if (!btn.is(':visible')) {
+        btn.show();
+      }
+    } else {
+      btn.hide();
+    }
+  });
 
 
-/*<div>
-    <nav class="navbar navbar-inverse" role="navigation">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="#">This is me</a></li>
-          <li><a href="#">My Skills</a></li>
-          <li><a href="#">My Work</a></li>
-          <li><a href="#">Conctact</a></li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </nav>
-  </div> <!-- /. menu -->*/
+  */
+
+
+
